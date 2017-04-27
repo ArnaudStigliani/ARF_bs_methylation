51a8ff60db5ad3f6f92cff4b7f0720de57a852c6
ArnaudStigliani <arnaud.stigliani@cea.fr>
2017-04-27 17:38:28 +0200

    scores.r prends tous les sites au dessus d'un seuil

--- scores.r
+++ scores.r
@@ -29,64 +29,69 @@ seq_pos <- as.character(ARF_pos)
 #-------------------------------------Compute Scores-----------------------------------------
 #
 threshold <-  maxScore(pwm_ARF) - 12
+#
 match_ARF <- sapply(FUN=matchPWM,seq_pos,pwm=pwm_ARF,min.score=threshold)
 nb_sites <- sapply(match_ARF,length)
-start_sites <- sapply(match_ARF,start)
+start_sites <- unlist(sapply(match_ARF,start))
+#
+match_ARF_rev <- sapply(FUN=matchPWM,seq_pos,pwm=pwm_ARF_rev,min.score=threshold)
+nb_sites_rev <- sapply(match_ARF_rev,length)
+start_sites_rev <- unlist(sapply(match_ARF_rev,start))
+#
+#
 
-scores_ARF_pos<- mapply(seq_pos,FUN=PWMscoreStartingAt,SIMPLIFY=TRUE,  starting.at=mapply(seq,1,width_pos-dim(pwm_ARF)[2],SIMPLIFY=FALSE),MoreArgs=list(pwm=pwm_ARF)) - maxScore(pwm_ARF)
-#
-scores_ARF_rev_pos<- mapply(seq_pos,FUN=PWMscoreStartingAt,SIMPLIFY=TRUE,  starting.at=mapply(seq,1,width_pos-dim(pwm_ARF)[2],SIMPLIFY=FALSE),MoreArgs=list(pwm=pwm_ARF_rev)) - maxScore(pwm_ARF)
-#
-max <- apply(FUN=max,scores_ARF_pos,2)
-max_rev<- apply(FUN=max,scores_ARF_rev_pos,2)
-#
-pos_max <- apply(FUN=which.max,scores_ARF_pos,2)
-pos_max_rev<- apply(FUN=which.max,scores_ARF_rev_pos,2)
-#
 seq_names<- names(seq_pos)
-list_seq_names <- unlist(str_split(seq_names,"[:-]"))
+#
+seq_names_plus <- rep(seq_names,times=nb_sites)
+seq_names_rev <- rep(seq_names,times=nb_sites_rev)
+#
+seq_names_rep <- c(seq_names_plus,seq_names_rev)
+#
+list_seq_names <- unlist(str_split(seq_names_rep,"[:-]"))
 tab_names <- matrix(list_seq_names,byrow=TRUE,ncol=3)
+tab_names <- cbind(tab_names,c(rep(1,length(seq_names_plus)),rep(-1,length(seq_names_rev))))
 chr_reg <- tab_names[,1]
+
 
 #-------------------------------------T1 -----------------------------------------
 
 offset <- 4
-pos <- ifelse(max > max_rev , pos_max + offset, pos_max_rev +11 - offset)
+pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
 pos_C <- as.integer(tab_names[,2])+pos
 T1 <- data.frame(chr_reg,pos_C,pos_C+1)
 
 #-------------------------------------G2 -----------------------------------------
 
 offset <- 5
-pos <- ifelse(max > max_rev , pos_max + offset, pos_max_rev +11 - offset)
+pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
 pos_C <- as.integer(tab_names[,2])+pos
 G2 <- data.frame(chr_reg,pos_C,pos_C+1)
 
 #-------------------------------------T3 -----------------------------------------
 
 offset <- 6
-pos <- ifelse(max > max_rev , pos_max + offset, pos_max_rev +11 - offset)
+pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
 pos_C <- as.integer(tab_names[,2])+pos
 T3 <- data.frame(chr_reg,pos_C,pos_C+1)
 
 #-------------------------------------C4 -----------------------------------------
 
 offset <- 7
-pos <- ifelse(max > max_rev , pos_max + offset, pos_max_rev +11 - offset)
+pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
 pos_C <- as.integer(tab_names[,2]) + pos
 C4 <- data.frame(chr_reg,pos_C,pos_C+1)
 
 #-------------------------------------G5 -----------------------------------------
 
 offset <- 8
-pos <- ifelse(max > max_rev , pos_max + offset, pos_max_rev +11 - offset)
+pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
 pos_C <- as.integer(tab_names[,2]) + pos
 G5 <- data.frame(chr_reg,pos_C,pos_C+1)
 
 #-------------------------------------G6 -----------------------------------------
 
 offset <- 9
-pos <- ifelse(max > max_rev , pos_max + offset, pos_max_rev +11 - offset)
+pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
 pos_C <- as.integer(tab_names[,2]) + pos
 G6 <- data.frame(chr_reg,pos_C,pos_C+1)
 
