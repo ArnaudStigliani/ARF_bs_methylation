c7090dd0bdd6b4ffbdaa9814def79672593aff66
ArnaudStigliani <arnaud.stigliani@cea.fr>
2017-04-28 12:38:13 +0200

    intersect_methylation.sh fonctionel, récupération des régions sans bs implémentée

--- intersect_methylation.sh
+++ intersect_methylation.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 
-intersectBed -a mC_all.bedGraph -b T1_ARF2_pos.bed > T1_C_all_met_ARF2_pos.bed &
-intersectBed -a mC_all.bedGraph -b G2_ARF2_pos.bed > G2_C_all_met_ARF2_pos.bed &
-intersectBed -a mC_all.bedGraph -b T3_ARF2_pos.bed > T3_C_all_met_ARF2_pos.bed &
-intersectBed -a mC_all.bedGraph -b C4_ARF2_pos.bed > C4_C_all_met_ARF2_pos.bed &
-intersectBed -a mC_all.bedGraph -b G5_ARF2_pos.bed > G5_C_all_met_ARF2_pos.bed &
-intersectBed -a mC_all.bedGraph -b G6_ARF2_pos.bed > G6_C_all_met_ARF2_pos.bed &
-intersectBed -a mC_all.bedGraph -b T1_ARF2_neg.bed > T1_C_all_met_ARF2_neg.bed &
-intersectBed -a mC_all.bedGraph -b G2_ARF2_neg.bed > G2_C_all_met_ARF2_neg.bed &
-intersectBed -a mC_all.bedGraph -b T3_ARF2_neg.bed > T3_C_all_met_ARF2_neg.bed &
-intersectBed -a mC_all.bedGraph -b C4_ARF2_neg.bed > C4_C_all_met_ARF2_neg.bed &
-intersectBed -a mC_all.bedGraph -b G5_ARF2_neg.bed > G5_C_all_met_ARF2_neg.bed &
-intersectBed -a mC_all.bedGraph -b G6_ARF2_neg.bed > G6_C_all_met_ARF2_neg.bed &
-intersectBed -a mC_all.bedGraph -b T1_ARF5_pos.bed > T1_C_all_met_ARF5_pos.bed &
-intersectBed -a mC_all.bedGraph -b G2_ARF5_pos.bed > G2_C_all_met_ARF5_pos.bed &
-intersectBed -a mC_all.bedGraph -b T3_ARF5_pos.bed > T3_C_all_met_ARF5_pos.bed &
-intersectBed -a mC_all.bedGraph -b C4_ARF5_pos.bed > C4_C_all_met_ARF5_pos.bed &
-intersectBed -a mC_all.bedGraph -b G5_ARF5_pos.bed > G5_C_all_met_ARF5_pos.bed &
-intersectBed -a mC_all.bedGraph -b G6_ARF5_pos.bed > G6_C_all_met_ARF5_pos.bed &
-intersectBed -a mC_all.bedGraph -b T1_ARF5_neg.bed > T1_C_all_met_ARF5_neg.bed &
-intersectBed -a mC_all.bedGraph -b G2_ARF5_neg.bed > G2_C_all_met_ARF5_neg.bed &
-intersectBed -a mC_all.bedGraph -b T3_ARF5_neg.bed > T3_C_all_met_ARF5_neg.bed &
-intersectBed -a mC_all.bedGraph -b C4_ARF5_neg.bed > C4_C_all_met_ARF5_neg.bed &
-intersectBed -a mC_all.bedGraph -b G5_ARF5_neg.bed > G5_C_all_met_ARF5_neg.bed &
-intersectBed -a mC_all.bedGraph -b G6_ARF5_neg.bed > G6_C_all_met_ARF5_neg.bed &
-
+intersectBed -wb -a mC_all.bedGraph -b T1_ARF2_pos.bed > T1_C_all_met_ARF2_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b G2_ARF2_pos.bed > G2_C_all_met_ARF2_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b T3_ARF2_pos.bed > T3_C_all_met_ARF2_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b C4_ARF2_pos.bed > C4_C_all_met_ARF2_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b G5_ARF2_pos.bed > G5_C_all_met_ARF2_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b G6_ARF2_pos.bed > G6_C_all_met_ARF2_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b T1_ARF2_neg.bed > T1_C_all_met_ARF2_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b G2_ARF2_neg.bed > G2_C_all_met_ARF2_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b T3_ARF2_neg.bed > T3_C_all_met_ARF2_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b C4_ARF2_neg.bed > C4_C_all_met_ARF2_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b G5_ARF2_neg.bed > G5_C_all_met_ARF2_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b G6_ARF2_neg.bed > G6_C_all_met_ARF2_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b T1_ARF5_pos.bed > T1_C_all_met_ARF5_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b G2_ARF5_pos.bed > G2_C_all_met_ARF5_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b T3_ARF5_pos.bed > T3_C_all_met_ARF5_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b C4_ARF5_pos.bed > C4_C_all_met_ARF5_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b G5_ARF5_pos.bed > G5_C_all_met_ARF5_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b G6_ARF5_pos.bed > G6_C_all_met_ARF5_pos.bed &
+intersectBed -wb -a mC_all.bedGraph -b T1_ARF5_neg.bed > T1_C_all_met_ARF5_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b G2_ARF5_neg.bed > G2_C_all_met_ARF5_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b T3_ARF5_neg.bed > T3_C_all_met_ARF5_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b C4_ARF5_neg.bed > C4_C_all_met_ARF5_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b G5_ARF5_neg.bed > G5_C_all_met_ARF5_neg.bed &
+intersectBed -wb -a mC_all.bedGraph -b G6_ARF5_neg.bed > G6_C_all_met_ARF5_neg.bed &
+wait
+sort -k1,1 -k2,2n sites_ARF5_neg.bed | mergeBed -i - | intersectBed -v -a ARF5_neg.bed -b - | intersectBed -a mC_all.bedGraph -b - > reg_wo_sites_C_all_met_ARF5_neg.bed &
+sort -k1,1 -k2,2n sites_ARF5_pos.bed | mergeBed -i - | intersectBed -v -a ARF5_pos.bed -b - | intersectBed -a mC_all.bedGraph -b - > reg_wo_sites_C_all_met_ARF5_pos.bed &
+sort -k1,1 -k2,2n sites_ARF2_neg.bed | mergeBed -i - | intersectBed -v -a ARF2_neg.bed -b - | intersectBed -a mC_all.bedGraph -b - > reg_wo_sites_C_all_met_ARF2_neg.bed &
+sort -k1,1 -k2,2n sites_ARF2_pos.bed | mergeBed -i - | intersectBed -v -a ARF2_neg.bed -b - | intersectBed -a mC_all.bedGraph -b - > reg_wo_sites_C_all_met_ARF2_pos.bed &
 exit 0
--- scores.r
+++ scores.r
@@ -13,93 +13,126 @@ pfm_ARF <- cbind(rep(151,4),pfm_ARF,rep(151,4))
 maxi_ARF <- apply(pfm_ARF,FUN=max, 2)
 maxi_ARF <- matrix(nrow=4, rep(maxi_ARF,4),byrow=TRUE)
 pwm_ARF <- log(pfm_ARF/maxi_ARF)
-pwm_ARF_rev <- pwm_ARF - minScore(pwm_ARF)/dim(pwm_ARF)[2] 
-pwm_ARF <-  reverseComplement(pwm_ARF_rev) ; pwm_ARF
+pwm_ARF2_rev <- pwm_ARF - minScore(pwm_ARF)/dim(pwm_ARF)[2] 
+pwm_ARF2 <-  reverseComplement(pwm_ARF2_rev) ; pwm_ARF
 
+pfm_ARF<- read.table("m_ARF5.txt",header=TRUE,sep="\t",skip=1)
+pfm_ARF <- round((t(as.matrix(pfm_ARF)))*nRegion)+1 ;pfm_ARF
+pfm_ARF <- cbind(rep(151,4),rep(151,4),pfm_ARF)
+maxi_ARF <- apply(pfm_ARF,FUN=max, 2)
+maxi_ARF <- matrix(nrow=4, rep(maxi_ARF,4),byrow=TRUE)
+pwm_ARF <- log(pfm_ARF/maxi_ARF)
+pwm_ARF5_rev <- pwm_ARF - minScore(pwm_ARF)/dim(pwm_ARF)[2] 
+pwm_ARF5 <-  reverseComplement(pwm_ARF5_rev) ; pwm_ARF
+
+#-------------------------------------Loop ------------------------------------------
+
+i <- 0
+for (elt in list("ARF5_pos","ARF5_neg","ARF2_pos","ARF2_neg"))
+{
+    i <- i+1
+    if (i==1 || i==2)
+    {
+        pwm_ARF <- pwm_ARF5
+        pwm_ARF_rev<- pwm_ARF5_rev
+        print("ARF5\n")
+    }
+    else
+    {
+        pwm_ARF <- pwm_ARF2
+        pwm_ARF_rev<- pwm_ARF2_rev
+        print("ARF2\n")
+    }   
 #-------------------------------------read fasta files-----------------------------------------
-tair10dir <- "~/Data/tair10/tair10.fas"
-tair10 <- readDNAStringSet(tair10dir)
-ARF_pos <- readDNAStringSet('ARF2_pos.fas')
+    ARF_pos <- readDNAStringSet(paste(elt,".fas",sep=""))
 
-width_pos <- width(ARF_pos)
-seq_pos <- as.character(ARF_pos)
+    width_pos <- width(ARF_pos)
+    seq_pos <- as.character(ARF_pos)
 
 
 
 #-------------------------------------Compute Scores-----------------------------------------
 #
-threshold <-  maxScore(pwm_ARF) - 12
+    threshold <-  maxScore(pwm_ARF) - 12
 #
-match_ARF <- sapply(FUN=matchPWM,seq_pos,pwm=pwm_ARF,min.score=threshold)
-nb_sites <- sapply(match_ARF,length)
-start_sites <- unlist(sapply(match_ARF,start))
+    match_ARF <- sapply(FUN=matchPWM,seq_pos,pwm=pwm_ARF,min.score=threshold)
+    nb_sites <- sapply(match_ARF,length)
+    start_sites <- unlist(sapply(match_ARF,start))
 #
-match_ARF_rev <- sapply(FUN=matchPWM,seq_pos,pwm=pwm_ARF_rev,min.score=threshold)
-nb_sites_rev <- sapply(match_ARF_rev,length)
-start_sites_rev <- unlist(sapply(match_ARF_rev,start))
+    match_ARF_rev <- sapply(FUN=matchPWM,seq_pos,pwm=pwm_ARF_rev,min.score=threshold)
+    nb_sites_rev <- sapply(match_ARF_rev,length)
+    start_sites_rev <- unlist(sapply(match_ARF_rev,start))
 #
 #
 
-seq_names<- names(seq_pos)
+    seq_names<- names(seq_pos)
 #
-seq_names_plus <- rep(seq_names,times=nb_sites)
-seq_names_rev <- rep(seq_names,times=nb_sites_rev)
+    seq_names_plus <- rep(seq_names,times=nb_sites)
+    seq_names_rev <- rep(seq_names,times=nb_sites_rev)
 #
-seq_names_rep <- c(seq_names_plus,seq_names_rev)
+    seq_names_rep <- c(seq_names_plus,seq_names_rev)
 #
-list_seq_names <- unlist(str_split(seq_names_rep,"[:-]"))
-tab_names <- matrix(list_seq_names,byrow=TRUE,ncol=3)
-tab_names <- cbind(tab_names,c(rep(1,length(seq_names_plus)),rep(-1,length(seq_names_rev))))
-chr_reg <- tab_names[,1]
+    list_seq_names <- unlist(str_split(seq_names_rep,"[:-]"))
+    tab_names <- matrix(list_seq_names,byrow=TRUE,ncol=3)
+    tab_names <- cbind(tab_names,c(rep(1,length(seq_names_plus)),rep(-1,length(seq_names_rev))))
+    chr_reg <- tab_names[,1]
 
 
 #-------------------------------------T1 -----------------------------------------
 
-offset <- 4
-pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
-pos_C <- as.integer(tab_names[,2])+pos
-T1 <- data.frame(chr_reg,pos_C,pos_C+1)
+    offset <- 4
+    pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
+    pos_C <- as.integer(tab_names[,2])+pos
+    T1 <- data.frame(chr_reg,pos_C,pos_C+1,tab_names[,4])
 
 #-------------------------------------G2 -----------------------------------------
 
-offset <- 5
-pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
-pos_C <- as.integer(tab_names[,2])+pos
-G2 <- data.frame(chr_reg,pos_C,pos_C+1)
+    offset <- 5
+    pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
+    pos_C <- as.integer(tab_names[,2])+pos
+    G2 <- data.frame(chr_reg,pos_C,pos_C+1,tab_names[,4])
 
 #-------------------------------------T3 -----------------------------------------
 
-offset <- 6
-pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
-pos_C <- as.integer(tab_names[,2])+pos
-T3 <- data.frame(chr_reg,pos_C,pos_C+1)
+    offset <- 6
+    pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
+    pos_C <- as.integer(tab_names[,2])+pos
+    T3 <- data.frame(chr_reg,pos_C,pos_C+1,tab_names[,4])
 
 #-------------------------------------C4 -----------------------------------------
 
-offset <- 7
-pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
-pos_C <- as.integer(tab_names[,2]) + pos
-C4 <- data.frame(chr_reg,pos_C,pos_C+1)
+    offset <- 7
+    pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
+    pos_C <- as.integer(tab_names[,2]) + pos
+    C4 <- data.frame(chr_reg,pos_C,pos_C+1,tab_names[,4])
 
 #-------------------------------------G5 -----------------------------------------
 
-offset <- 8
-pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
-pos_C <- as.integer(tab_names[,2]) + pos
-G5 <- data.frame(chr_reg,pos_C,pos_C+1)
+    offset <- 8
+    pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
+    pos_C <- as.integer(tab_names[,2]) + pos
+    G5 <- data.frame(chr_reg,pos_C,pos_C+1,tab_names[,4])
 
 #-------------------------------------G6 -----------------------------------------
 
-offset <- 9
-pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
-pos_C <- as.integer(tab_names[,2]) + pos
-G6 <- data.frame(chr_reg,pos_C,pos_C+1)
+    offset <- 9
+    pos <- ifelse(tab_names[,4] ==1 , start_sites + offset, start_sites_rev +11 - offset)
+    pos_C <- as.integer(tab_names[,2]) + pos
+    G6 <- data.frame(chr_reg,pos_C,pos_C+1,tab_names[,4])
 
+#-------------------------------------sites -----------------------------------------
 
-write.table(T1,"T1_ARF2_pos.bed",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
-write.table(G2,"G2_ARF2_pos.bed",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
-write.table(T3,"T3_ARF2_pos.bed",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
-write.table(C4,"C4_ARF2_pos.bed",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
-write.table(G5,"G5_ARF2_pos.bed",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
-write.table(G6,"G6_ARF2_pos.bed",quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    offset <- 3
+    pos <-  start_sites + offset
+    pos_begin <- as.integer(tab_names[,2]) + pos
+    sites <- data.frame(chr_reg,pos_begin,pos_begin + 6)
 
+    options(scipen=999)
+    write.table(T1,paste("T1_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    write.table(G2,paste("G2_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    write.table(T3,paste("T3_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    write.table(C4,paste("C4_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    write.table(G5,paste("G5_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    write.table(G6,paste("G6_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+    write.table(sites,paste("sites_",elt,".bed",sep=""),quote=FALSE,sep="\t",row.names=FALSE,col.names=FALSE)
+}
